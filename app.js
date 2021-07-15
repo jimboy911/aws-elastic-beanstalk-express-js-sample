7d307be084aac52a3bc22a9d501d5143fffb4c58
root <root@ip-172-31-48-186.ec2.internal>
2021-07-15 10:26:40 +0000

    changed message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Celito!'));
+app.get('/', (req, res) => res.send('Hello Dudu July 15 2021!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
