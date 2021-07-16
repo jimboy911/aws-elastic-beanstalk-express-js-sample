98fbf3fb86313cf9f9044c95d6047aca0c23278f
root <root@ip-172-31-48-186.ec2.internal>
2021-07-16 11:01:32 +0000

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Dudu July 15 2021!'));
+app.get('/', (req, res) => res.send('Hello Dudu July 16 2021!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
